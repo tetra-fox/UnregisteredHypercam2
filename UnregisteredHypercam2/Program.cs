b47e94f81f91364b1068d12598f7a0a2b9875390
Tetra <wwc.302@gmail.com>
2017-10-22 16:49:55 -0400

    add exit button

--- UnregisteredHypercam2/Program.cs
+++ UnregisteredHypercam2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Windows.Forms;
+using System.Drawing;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
 using UnregisteredHypercam2.Properties;
 
 namespace UnregisteredHypercam2
@@ -18,7 +19,7 @@ namespace UnregisteredHypercam2
 
         private static void Main()
         {
-            // create form
+            // create Form f
             var f = new Form
             {
                 FormBorderStyle = FormBorderStyle.None,
@@ -28,8 +29,8 @@ namespace UnregisteredHypercam2
                 MinimumSize = Resources.unregistered.Size,
                 MaximumSize = Resources.unregistered.Size
             };
-            
-            // create picturebox
+
+            // create PictureBox p
             var p = new PictureBox
             {
                 Image = Resources.unregistered,
@@ -37,15 +38,36 @@ namespace UnregisteredHypercam2
                 Height = Resources.unregistered.Height
             };
 
-            // add picturebox to form
+            // create ContextMenu c for future NotifyIcon n
+            var c = new ContextMenu();
+            c.MenuItems.Add("exit").Click += ExitButtonOnClick;
+
+            // create NotifyIcon n (n not used anywhere, just there for consistency/readability)
+            var n = new NotifyIcon
+            {
+                Text = "UnregisteredHypercam2",
+                Visible = true,
+                // i'm too lazy to make an icon lmao
+                Icon = SystemIcons.Application,
+                ContextMenu = c
+            };
+
+            // add PictureBox p to Form f
             f.Controls.Add(p);
 
-            // allow click-through & hide from alt+tab menu
-            SetWindowLong(f.Handle, -20, GetWindowLong(f.Handle, -20) | 0x80000 | 0x20 | 0x80);
-            SetLayeredWindowAttributes(f.Handle, 0, 255, 0x2);
+            // window styling (need to create a transparent window because click through doesn't work if it's not transparent)
+            SetWindowLong(f.Handle, -20, GetWindowLong(f.Handle, -20) | 0x80000 | 0x20 | 0x80); // Form f, GWL_EXSTYLE | WX_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOOLWINDOW
+            SetLayeredWindowAttributes(f.Handle, 0, 255, 0x2); // 0x2 = LWA_ALPHA
 
-            // run
+            // run with Form f
             Application.Run(f);
+        }
+
+        // exit button event handler
+        private static void ExitButtonOnClick(object sender, EventArgs eventArgs)
+        {
+            // exit
+            Application.Exit();
         }
     }
 }
--- UnregisteredHypercam2/Properties/AssemblyInfo.cs
+++ UnregisteredHypercam2/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -9,8 +10,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnregisteredHypercam2")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +21,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3a09dc90-fc48-4426-9842-77f6f9348db6")]
+[assembly: Guid("778c3471-5fcb-4b7d-a556-e3385b8854a9")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// fuck you i do what i want
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
+
